f7b81b37d75da3d1e0e95b7172fb1a68a9a67553
Redi Joy Davila Cabral <N00459742@upn.pe>
2025-04-13 18:45:59 -0500

    Primer commit

---
+++ Tarea1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Definimos variables
+                double Datos, Tiempo, Banda;
+                //Ingresamos los datos
+                Console.WriteLine("Ingrese la cantidad de datos a tranferir (en MB)");
+                Datos = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el tiempo disponible para la transferencia(en segundos)");
+                Tiempo = double.Parse(Console.ReadLine());
+                //Proceso
+                Banda = Math.Round((Datos * 8) / Tiempo, 2);
+                //Salida mostrando el resultado
+                Console.WriteLine("El ancho de banda necesario es: " + Banda + "Mbps");
+                Console.ReadKey();
+        }
+    }
+}
